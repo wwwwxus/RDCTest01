476b6d8be25b230e7220abbdba0269736fd2ba3f
Mia <2976370121@qq.com>
2024-03-21 20:58:12 +0800

    kaohe1

--- document/test1.js
+++ document/test1.js
@@ -33,42 +33,111 @@ for (let i = 0; i < color.length; i++) {
         }
     })
 }
-const right = document.querySelector('.note13 .tip1')
-const left = document.querySelector('.note13 .tip2')
-const imgs = document.querySelector('.note13 .Bigbox .IMGS13')
+const note13right = document.querySelector('.note13 .tip1')
+const note13left = document.querySelector('.note13 .tip2')
+const note13imgs = document.querySelector('.note13 .Bigbox .IMGS13')
 let i = 0
 // console.log(i);
-left.style.display = 'none'
-function render(){
+note13left.style.display = 'none'
+function note13render(){
     if(i === 0){
-        imgs.style.transform = 'translate(0px, 0px)'
-        left.style.display = 'none'
-        right.style.display = 'block'
+        note13imgs.style.transform = 'translate(0px, 0px)'
+        note13left.style.display = 'none'
+        note13right.style.display = 'block'
     }
     if(i === 1){
-        imgs.style.transform = 'translate(-700px, 0px)'
-        left.style.display = 'block'
-        right.style.display = 'block'
+        note13imgs.style.transform = 'translate(-700px, 0px)'
+        note13left.style.display = 'block'
+        note13right.style.display = 'block'
     }
     if(i === 2){
-        imgs.style.transform = 'translate(-1400px, 0px)'
-        left.style.display = 'block'
-        right.style.display = 'block'
+        note13imgs.style.transform = 'translate(-1400px, 0px)'
+        note13left.style.display = 'block'
+        note13right.style.display = 'block'
     }
     if(i === 3){
-        imgs.style.transform = 'translate(-2000px, 0px)'
-        right.style.display = 'none'
-        left.style.display = 'block'
+        note13imgs.style.transform = 'translate(-2000px, 0px)'
+        note13right.style.display = 'none'
+        note13left.style.display = 'block'
     }
 }
-right.addEventListener('click',function(){
+note13right.addEventListener('click',function(){
     i++
     // console.log(i)
-    render()
+    note13render()
 })
-left.addEventListener('click', function () {
+note13left.addEventListener('click', function () {
     i--
     // console.log(i)
-    render()
+    note13render()
 })
 
+const note27right = document.querySelector('.note27 .tip1')
+const note27left = document.querySelector('.note27 .tip2')
+const note27imgs = document.querySelector('.note27 .Bigbox .IMGS27')
+let k = 0
+// console.log(k);
+note27left.style.display = 'none'
+function note27render() {
+    if (k === 0) {
+        note27imgs.style.transform = 'translate(0px, 0px)'
+        note27left.style.display = 'none'
+        note27right.style.display = 'block'
+    }
+    if (k === 1) {
+        note27imgs.style.transform = 'translate(-600px, 0px)'
+        note27left.style.display = 'block'
+        note27right.style.display = 'block'
+    }
+    if (k === 2) {
+        note27imgs.style.transform = 'translate(-1200px, 0px)'
+        note27left.style.display = 'block'
+        note27right.style.display = 'none'
+    }
+
+}
+note27right.addEventListener('click', function () {
+    k++
+    // console.log(k)
+    note27render()
+})
+note27left.addEventListener('click', function () {
+    k--
+    // console.log(k)
+    note27render()
+})
+
+const note28right = document.querySelector('.note28 .tip1')
+const note28left = document.querySelector('.note28 .tip2')
+const note28imgs = document.querySelector('.note28 .Bigbox .IMGS28')
+let j = 0
+// console.log(j);
+note28left.style.display = 'none'
+function note28render() {
+    if (j === 0) {
+        note28imgs.style.transform = 'translate(0px, 0px)'
+        note28left.style.display = 'none'
+        note28right.style.display = 'block'
+    }
+    if (j === 1) {
+        note28imgs.style.transform = 'translate(-600px, 0px)'
+        note28left.style.display = 'block'
+        note28right.style.display = 'block'
+    }
+    if (j === 2) {
+        note28imgs.style.transform = 'translate(-1200px, 0px)'
+        note28left.style.display = 'block'
+        note28right.style.display = 'none'
+    }
+
+}
+note28right.addEventListener('click', function () {
+    j++
+    // console.log(j)
+    note28render()
+})
+note28left.addEventListener('click', function () {
+    j--
+    // console.log(j)
+    note28render()
+})
